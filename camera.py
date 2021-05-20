0c41f01e22043c6bd2ed938170643635516a8232
Katerina Holan <katholan@gmail.com>
2021-05-20 13:35:13 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,43 +1,26 @@
-###using python
+#Once you're connected to your Pi, click the logo icon in the upper left, then programming, then Thonny Python IDE
+#Copy/Paste the following code into the script and save as camera.py
+#You can also move this onto the microSD card.
+#You can run the code from the python shell in Thonny or from the Pi command prompt.
+#Make sure your camera is enabled by going to home/preferences/raspberry configurations/interfaces/enable camera
+#Make sure your setup is complete and your camera is in the right position before running. 
+#It starts with a 0 hour photo that will take almost immediately after running.
+#Use this photo to ensure your setup is correct before disconnecting your laptop/monitor.
+
 from picamera import PiCamera
 from time import sleep
+import os
+
+#set working directory where you want the photos to be saved.
+os.chdir('/home/pi/kat_holan/test')
 
 #setting fixed attributes to the camera will allow for consistency between photos, i.e. no auto-adjustment
-camera = PiCamera(resolution=(1920, 1080), framerate=30)
-camera.iso = 100 #this value sets sensitivity to light. Lower values are less noisy but perform worse in low light conditions.
-
+camera = PiCamera(resolution=(1920, 1080), framerate=30) #the max for stills with the V2 camera is 3280 × 2464
+camera.iso = 100 #this value sets sensitivity to light. Lower values are less noisy but perform worse in low light conditions. Since this setup uses constant external light we can keep this value low
 
 camera.start_preview()
 sleep(5) #important to sleep for a few seconds to adjust to light levels
-for i in range(48): #will save a total of 48 photos
+for i in range(49): #will save a total of 49 photos
+	camera.capture('immune_suppression_{0}_hour.jpg'.format(i)) #will save the file with the HPI from 0 to 48.
 	sleep(3600) #wait 1 hour between photos
-	camera.capture('immune_suppression_{timestamp:%m-%d-%Y-%H-%M}_' % i 'hour.jpg') #will save the file with current date and time and HPI
 camera.stop_preview()
-
-
-
-
-###practice with short time intervals
-from picamera import PiCamera
-from time import sleep
-
-#setting fixed attributes to the camera will allow for consistency between photos, i.e. no auto-adjustment
-camera = PiCamera(resolution=(1920, 1080), framerate=30)
-camera.iso = 100 #this value sets sensitivity to light. Lower values are less noisy but perform worse in low light conditions.
-camera.start_preview()
-sleep(5) #important to sleep for a few seconds to adjust to light levels
-for i in range(48): #will save a total of 48 photos
-	sleep(5) #wait 1 hour between photos
-	camera.capture('immune_suppression_{timestamp:%m-%d-%Y-%H-%M}_' % i 'hour.jpg') #will save the file with current date and time and HPI
-#camera.stop_preview()
-
-
-
-
-###alternative option using command line raspistill
-#time is in milliseconds
-#t = total period of time to take photos
-#tl = take a picture every interval of this size
-#this line takes a photos every hour for 48 hours and saves them with a time stamp and HPI
-DATE=$(date+"%m-%d-%Y_%H-%M")
-raspistill -t 172800000 -tl 3600000 -o immune_suppression_$DATE_%03d.jpg
